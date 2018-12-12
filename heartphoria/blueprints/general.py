a6c239f21be9648fb08645de94b8b2d2e4f4ba83
Joel <joel.jdesignera@gmail.com>
2018-12-13 07:54:39 +0800

    3.0.1-dev Major Re-structure.

--- heartphoria/blueprints/general.py
+++ heartphoria/blueprints/general.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template
+
+from heartphoria.db import get_db
 
 blueprint = Blueprint('general', __name__)
 
 @blueprint.route('/')
 def index():
-    year=None
-    return render_template('index.html', title='Home Page', year=year)
+    reminders = None
+    appointments = None
+
+    if g.user:
+        db = get_db()
+
+        reminders = db.execute('SELECT * FROM reminder WHERE user_id = ? ORDER BY time LIMIT 5', [g.user['id']]).fetchall()
+        appointments = db.execute('SELECT * FROM appointment WHERE user_id = ? AND date_time > datetime("now") ORDER BY date_time DESC LIMIT 5', [g.user['id']]).fetchall()
+
+    return render_template('index.html', title='Home Page', reminders=reminders, appointments=appointments)
