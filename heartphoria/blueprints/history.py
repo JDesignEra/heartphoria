0ab3c4044c94138280c3d61aeb3fa39c06450e37
Dylan <layzeeisme@gmail.com>
2018-12-14 20:49:58 +0800

    4.0.4-dev

--- heartphoria/blueprints/history.py
+++ heartphoria/blueprints/history.py
@@ -8,5 +8,27 @@ blueprint = Blueprint('history', __name__, url_prefix='/history')
 @blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    date = None
+    description = None
+    errors = {}
 
-    return render_template('history/index.html')
+    db = get_db()
+
+    if request.method == 'POST':
+        date = request.form.get('date')
+        description = request.form.get('description')
+
+        if not date:
+            errors['date'] = 'Date is required.'
+
+        if not description:
+            errors['description'] = 'Description is required.'
+
+        if not errors:
+            db.execute('INSERT INTO history (user_id, date, description) VALUES (?, ?, ?)',
+                       [g.user['id'], date, description])
+            db.commit()
+
+    histories = db.execute('SELECT * FROM history WHERE user_id = ? ORDER BY date DESC', [g.user['id']]).fetchall()
+
+    return render_template('history/index.html', date=date, description=description, errors=errors, histories=histories)
