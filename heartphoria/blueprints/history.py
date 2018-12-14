5b7564f77b142298c51bb2dc9bf4b945a17d63ed
Dylan <layzeeisme@gmail.com>
2018-12-14 20:34:50 +0800

    3.0.11-dev

--- heartphoria/blueprints/history.py
+++ heartphoria/blueprints/history.py
@@ -21,6 +21,9 @@ def index():
         if not date:
             errors['date'] = 'Date is required.'
 
+        if not description:
+            errors['description'] = 'Description is required.'
+
     histories = db.execute('SELECT * FROM history WHERE user_id = ? ORDER BY date DESC', [g.user['id']]).fetchall()
 
     return render_template('history/index.html', date=date, description=description, errors=errors, histories=histories)
