e2c25008ff9cf466ba25e838630775a97d1e9a49
Dylan <layzeeisme@gmail.com>
2018-12-14 20:33:59 +0800

    3.0.11-dev

--- heartphoria/blueprints/history.py
+++ heartphoria/blueprints/history.py
@@ -18,18 +18,6 @@ def index():
         date = request.form.get('date')
         description = request.form.get('description')
 
-        if not date:
-            errors['date'] = 'Date is required.'
-
-        if not description:
-            errors['description'] = 'Description is required.'
-            
-        if not errors:
-            db.execute('INSERT INTO history (user_id, date, description) VALUES (?, ?, ?)', [g.user['id'], date, description])
-            db.commit()
-
-            return redirect(url_for('.index'))
-
     histories = db.execute('SELECT * FROM history WHERE user_id = ? ORDER BY date DESC', [g.user['id']]).fetchall()
 
     return render_template('history/index.html', date=date, description=description, errors=errors, histories=histories)
